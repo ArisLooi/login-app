58f5813689c9ceb5537cc7d4cd8177e4b23d8eec
Aris Looi <arislooi@outlook.com>
2024-12-06 14:04:26 +0800

    add AuthContext.js and RequireAuth.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../AuthContext';
 
 export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const authContext = useContext(AuthContext);
+
+    function login() {
+        const isCorrectUsername = username === "haris@sigmaschool.co";
+        const isCorrectPassword = password === "password";
+        if (isCorrectUsername && isCorrectPassword) {
+            authContext.setToken("1234");
+            navigate("/dashboard");
+        }
+    }
 
     return (
         <Container>
@@ -12,7 +25,11 @@ export default function Login() {
             <Form>
                 <Form.Group className='mb-3' controlId='formBasicEmail'>
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
                     <Form.Text className='text-muted'>
                         We&apos;ll never share your email with anyone else.
                     </Form.Text>
@@ -20,11 +37,14 @@ export default function Login() {
 
                 <Form.Group className='mb-3' controlId='formBasicPassword'>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Form.Control
+                        type="password"
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
 
-                <Button variant="primary">Login</Button>
-
+                <Button variant="primary" onClick={login}>Login</Button>
             </Form>
         </Container>
     )
